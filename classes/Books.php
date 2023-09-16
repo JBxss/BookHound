37c505f5eaf65a4f069ade0f5da1de747d1fb3dc
JBxss <juanseb100@gmail.com>
2023-09-16 09:47:47 -0500

    [ADD] Validaiones Metodo GET

--- classes/Books.php
+++ classes/Books.php
@@ -6,25 +6,45 @@ class Books
 {
     function obtenerLibros()
     {
-        $db = Flight::db();
-        $query = $db->prepare("SELECT * FROM tbl_libros");
-        $query->execute();
-        $data = $query->fetchAll();
-        $array = [];
+        try {
+            $db = Flight::db();
+            $query = $db->prepare("SELECT * FROM tbl_libros");
+            $query->execute();
+            $data = $query->fetchAll();
 
-        foreach ($data as $row) {
-            $array[] = [
-                "Nombre" => $row['nombre_libro'],
-                "Autor" => $row['autor_libro'],
-                "Fecha de Publicacion" => $row['fecha_libro'],
-                "Categoria" => $row['categoria_libro']
-            ];
+            if ($query->rowCount() === 0) {
+                Flight::halt(404, json_encode(
+                    [
+                        "error" => "No se encontraron libros",
+                        "status" => "error",
+                        "code" => "404"
+                    ]
+                ));
+            } else {
+                $array = [];
+
+                foreach ($data as $row) {
+                    $array[] = [
+                        "Nombre" => $row['nombre_libro'],
+                        "Autor" => $row['autor_libro'],
+                        "Fecha de Publicacion" => $row['fecha_libro'],
+                        "Categoria" => $row['categoria_libro']
+                    ];
+                }
+
+                Flight::json([
+                    "total_rows" => $query->rowCount(),
+                    "rows" => $array
+                ]);
+            }
+        } catch (PDOException $e) {
+            Flight::halt(500, json_encode(
+                [
+                    "error" => "Error en la consulta SQL: " . $e->getMessage(),
+                    "status" => "error",
+                    "code" => "500"
+                ]
+            ));
         }
-
-
-        Flight::json([
-            "total_rows" => $query->rowCount(),
-            "rows" => $array
-        ]);
     }
 }
