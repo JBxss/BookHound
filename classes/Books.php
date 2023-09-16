9c7f03a98aef3d72dbff8b41a079ec35fa0216b5
JBxss <juanseb100@gmail.com>
2023-09-16 09:36:43 -0500

    [ADD] Metodo GET

--- classes/Books.php
+++ classes/Books.php
@@ -4,5 +4,27 @@ require 'vendor/autoload.php';
 
 class Books
 {
+    function obtenerLibros()
+    {
+        $db = Flight::db();
+        $query = $db->prepare("SELECT * FROM tbl_libros");
+        $query->execute();
+        $data = $query->fetchAll();
+        $array = [];
 
+        foreach ($data as $row) {
+            $array[] = [
+                "Nombre" => $row['nombre_libro'],
+                "Autor" => $row['autor_libro'],
+                "Fecha de Publicacion" => $row['fecha_libro'],
+                "Categoria" => $row['categoria_libro']
+            ];
+        }
+
+
+        Flight::json([
+            "total_rows" => $query->rowCount(),
+            "rows" => $array
+        ]);
+    }
 }
--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 require 'vendor/autoload.php';
 require 'classes/Books.php';
 
-Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=API', 'root', ''));
+Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=APILibros', 'root', ''));
 
 $books = new Books();
 
-Flight::route('GET /books', [$books, 'resgistrar']);
-Flight::route('GET /books', [$books, 'resgistrar']);
-Flight::route('POST /books', [$books, 'resgistrar']);
-Flight::route('PUT /books', [$books, 'resgistrar']);
-Flight::route('DELETE /books', [$books, 'resgistrar']);
+Flight::route('GET /books', [$books, 'obtenerLibros']);
+// Flight::route('GET /books', [$books, '']);
+// Flight::route('POST /books', [$books, '']);
+// Flight::route('PUT /books', [$books, '']);
+// Flight::route('DELETE /books', [$books, '']);
 
-// permite buscar libros en función de un criterio específico.
-Flight::route('GET /books', [$books, 'resgistrar']);
+// // permite buscar libros en función de un criterio específico.
+// Flight::route('GET /books', [$books, '']);
 
 Flight::start();
